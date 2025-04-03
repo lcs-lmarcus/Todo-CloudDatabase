47cb53952615f4415a9ce10b99cc4267c7346fb6
Marcus Li <mmli@lcs.on.ca>
2025-04-03 09:37:46 -0400

    Adjusted the view model so that newly added to-do items are persisted.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,15 @@
+//
+//  Supabase.swift
+//  TodoList
+//
+//  Created by Marcus Li on 2025-04-03.
+//
+
+import Foundation
+import Supabase
+ 
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://rtykmiriwrjduolkjjok.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0eWttaXJpd3JqZHVvbGtqam9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MDc4MzksImV4cCI6MjA1OTE4MzgzOX0.0Ym8sKSNV_VOqxSPe89keXfS1HKgqfWZXYKm9WVLp9Y"
+)
+ 
